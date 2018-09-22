6018a74f4d43096b6e49126183f01f8c39b05991
albincorreya <albin.a.correya@gmail.com>
2018-09-22 12:58:18 +0200

    add_volume_mount in docker container

--- launchDocker.sh
+++ launchDocker.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+	echo "USAGE: ./launchDocker.sh <scripts_folder_path> "
+	exit
+fi
+
 echo "Building docker image"
 docker build -t hamrdocker .
 
 echo "Launching docker.."
-docker run -it --rm --name hamr_docker hamrdocker bash
+docker run -it --rm --name hamr_docker  -v $1:/srv/workspace/ hamrdocker bash
+
